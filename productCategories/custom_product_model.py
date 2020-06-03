d7a238f23eb405353d6fd11eb5e05c0d4bd012e0
Sudish Shrestha <sudishrestha@gmail.com>
2020-06-03 16:32:09 +0545

    Product Category module for product classification as in existing system of openerp

---
+++ productCategories/custom_product_model.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models,osv, fields, api, _
+from datetime import datetime, timedelta
+from odoo.exceptions import ValidationError
+class ProductCustomizeTask(models.Model):
+	_inherit = 'product.template'
+	orphan_drug = fields.Boolean('Orphan Drug')
+	org = fields.Boolean('Org')
+	bh = fields.Boolean('BH')
+	ch = fields.Boolean('CH')
+	x_low_cost_eq = fields.Boolean('MOHP Essesntial Medicine')
+	x_govt = fields.Boolean('Government Supply')
+	x_formulary = fields.Boolean('Formulary')
+	medicine_item = fields.Boolean('Medicine')
+	antibiotic = fields.Boolean('Antibiotics')
+	lab_item = fields.Boolean('Lab Item')
+	physic_medicine = fields.Boolean('Physic Medicine')
+	insurance_medicine = fields.Boolean('Insurance Medicine')
+	vertical_program = fields.Boolean('Vertical Program')
+	dental_item = fields.Boolean('Dental Item')
+	medical_item = fields.Boolean('Medical Item')
+	other_item = fields.Boolean('Other Item')

b6ab808d0b56421b7ab4e35e6d9be750bfb4ed12
root <root@localhost.localdomain>
2020-10-16 11:19:54 +0545

    delivery slip fixes

--- odooCustomization/models/stock_picking.py
+++ odooCustomization/models/stock_picking.py
@@ -8,18 +8,24 @@ _logger = logging.getLogger(__name__)
 class Picking(models.Model):
     _inherit = "stock.picking"
     x_amount_total = fields.Float(string='Total Amount', compute='get_x_total')
+    y_amount_total = fields.Float(string='Shipped Amount', compute='get_x_total')
     
 
 
     @api.onchange('move_lines')
-    def get_x_total(self):  
-        _logger.info("Inside get total stock picking")
-        total_amnt = 0
-        for product in self.move_lines:
-            total_amnt =total_amnt + (product.product_uom_qty * product.product_id.list_price)
-            # total_amnt + (product.price_unit * product.product_uom_qty )
+    def get_x_total(self):
+        purchase_data = self.env['purchase.order'].search([('name', '=', self.origin)], limit=1) 
+        total_amnt=purchase_data.amount_total 
+        ship_amnt = 0
+        for order in purchase_data.order_line:
+            for moves in self.move_lines:
+                if moves.product_id == order.product_id:
+                    ship_amnt= ship_amnt + order.price_subtotal
+        
         self.x_amount_total=total_amnt
+        self.y_amount_total=ship_amnt
         self.update({
-                        'x_amount_total':  total_amnt
+                        'x_amount_total':  total_amnt,
+                        'y_amount_total':  ship_amnt
                         })
-        return {'value': {'x_amount_total':  total_amnt}}
+        return {'value': {'x_amount_total':  total_amnt,'y_amount_total':  ship_amnt}}
